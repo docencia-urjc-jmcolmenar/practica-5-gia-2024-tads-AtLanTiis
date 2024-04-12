1352b15178b2502e4e230787a606eea7f7b2d87a
AtLanTiis <rafamatarivas@gmail.com>
2024-04-12 10:37:50 +0200

    Added local libraries and full functionality
    Added arrayplustad.c and arrayplustad.h. Added full functionality to main.c

--- main.c
+++ main.c
@@ -4,22 +4,27 @@
  * Descomenta progresivamente el código del archivo main.c y comprueba que el programa funciona correctamente.
  */
 
-//#include "arrayplustad.h"
+#include "arrayplustad.h"
 #define TAM 5
 
 /*
  * Programa para demostrar el uso del TAD ArrayPlus.
  */
 int main() {
-
-/*
     // Crea el ArrayPlus vacío
     ArrayPlusTAD *miArrayPlus = creaArrayPlus(TAM * 2);
 
+    // Añade un elemento al ArrayPlus
+    addElemento(miArrayPlus, 8);
+
     // Rellena el ArrayPlus con valores individuales
     for (int i = 0; i < TAM; i++) {
-        anyadeElemento(miArrayPlus, i);
+        addElemento(miArrayPlus, i);
     }
+
+    // Añade varios elementos al ArrayPlus
+    int arrayAux[] = {15, 64, 27, 99};
+    addArray(miArrayPlus, arrayAux, 4);
 
     // Muestra el contenido del ArrayPlus
     imprimeArrayPlus(miArrayPlus);
@@ -27,18 +32,11 @@ int main() {
     // Vacía el array
     vaciaArrayPlus(miArrayPlus);
 
-    // Añade varios elementos al ArrayPlus
-    int arrayAux[] = {15, 64, 27, 99};
-    anyadeArray(miArrayPlus, arrayAux, 4);
-
-    // Añade un elemento al ArrayPlus
-    anyadeElemento(miArrayPlus, 8);
-
     // Muestra el contenido del ArrayPlus
     imprimeArrayPlus(miArrayPlus);
 
     // Libera la memoria del ArrayPlus
     liberaArrayPlus(miArrayPlus);
-*/
+
     return 0;
 }
